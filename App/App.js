1e12019203fda64b37db44641c5c90108ede5d49
Brendon <brendonwbw@gmail.com>
2018-11-03 00:35:29 -0700

    Add react-native-elements and react-navigation

--- App/App.js
+++ App/App.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Button, ButtonGroup, Icon } from 'react-native-elements';
 
 export default class App extends React.Component {
   render() {
+
+    const footer = ['Profile', 'Connec', 'Friends']
+    
+    
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
+        <Button
+          icon={{
+            name: 'share',
+            size: 15,
+            color: 'white'
+          }}
+          title='Share User'
+        />
+        <ButtonGroup style={styles.footer}
+          buttons={footer}
+          containerStyle={{height:100}}
+        />
+      
       </View>
+
+      
     );
   }
 }
@@ -18,4 +38,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  footer: {
+    flex: 30,
+  }
 });
