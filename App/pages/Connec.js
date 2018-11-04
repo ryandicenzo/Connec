4cd639655ce9f68e6af67a1c78ff1de20357c2c0
Brendon <brendonwbw@gmail.com>
2018-11-03 17:38:18 -0700

    Delete stylesheet

--- App/pages/Connec.js
+++ App/pages/Connec.js
@@ -42,39 +42,6 @@ class Connec extends React.Component {
   }
 }
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//     backgroundColor: '#000',
-//   },
-//   bottomBar: {
-//     position: 'absolute',
-//     bottom: 0,
-//     left: 0,
-//     right: 0,
-//     backgroundColor: 'rgba(0,0,0,0.5)',
-//     padding: 15,
-//     flexDirection: 'row',
-//   },
-//   url: {
-//     flex: 1,
-//   },
-//   urlText: {
-//     color: '#fff',
-//     fontSize: 20,
-//   },
-//   cancelButton: {
-//     marginLeft: 10,
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   cancelButtonText: {
-//     color: 'rgba(255,255,255,0.8)',
-//     fontSize: 18,
-//   },
-// });
 
 export default connect(
   state => state
