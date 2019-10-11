ac3113345dc3bf5b94015fb5baca5b221d793fad
Brendon <brendonwbw@gmail.com>
2019-10-11 00:51:39 -0700

    Add interactive controls to camera and require that QR code is vCard

--- App/tabs/Camera.js
+++ App/tabs/Camera.js
@@ -25,35 +25,67 @@ export default class BarcodeScannerExample extends React.Component {
     const { hasCameraPermission, scanned } = this.state;
 
     if (hasCameraPermission === null) {
-      return <Text>Requesting for camera permission</Text>;
+      return <Text>Requesting camera permission</Text>;
     }
     if (hasCameraPermission === false) {
       return <Text>No access to camera</Text>;
     }
     return (
-      <View
-        style={{
-          flex: 1,
-          flexDirection: 'column',
-          justifyContent: 'flex-end',
-        }}>
+      <View style={styles.container}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : this.handleBarCodeScanned}
           style={StyleSheet.absoluteFillObject}
         />
+        
+        <View style={styles.bottomBar}>
+          
+          {/* Show if scanned */}
+          {scanned && (<Button color="#2E728F" title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />)}
+          
+          {/* Show if not scanned */}
+          {!scanned && (
+            <Text style ={styles.label}>
+              Hold Camera over QR Code
+            </Text>
+          )}
+          
+        </View>
 
-        {scanned && (
-          <Button title={'Tap to Scan Again'} onPress={() => this.setState({ scanned: false })} />
-        )}
       </View>
     );
   }
 
   handleBarCodeScanned = ({ type, data }) => {
-    this.setState({ scanned: true });
-    FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'savedfile.vcf');
-    const result = Share.share({
-      url: FileSystem.documentDirectory + 'savedfile.vcf'
-    });
+    if (data.slice(0,11) === 'BEGIN:VCARD') {
+      this.setState({ scanned: true });
+      FileSystem.writeAsStringAsync(FileSystem.documentDirectory + 'contact.vcf');
+      const result = Share.share({
+        url: FileSystem.documentDirectory + 'contact.vcf'
+      });
+    }
   };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
+  bottomBar: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'white',
+    padding: 30,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  label: {
+    fontSize: 18,
+    color: '#2E728F',
+    padding: 8,
+  },
+});
